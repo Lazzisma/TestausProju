33663e90f2caa8aa011f380532c4b92b6d76263c
Lassi Ollinen <lassi.ollinen@tuni.fi>
2022-12-12 04:58:53 +0200

    Unohtu updatee myös yksikkötesti

--- test/ceil.test.js
+++ test/ceil.test.js
@@ -1,6 +1,6 @@
 //import { jest } from '@jest/globals';
 //const ceil = require('../COMP.SE.200-2022-2023-1/src/ceil')
-import ceil from '../COMP.SE.200-2022-2023-1/src/ceil'
+import ceil from '../COMP.SE.200-2022-2023/src/ceil'
 
 //import ceil from '../COMP.SE.200-2022-2023-1/src/ceil.js'
  
